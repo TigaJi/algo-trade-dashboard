b40dd27db29dc70489252434b75f87a04e908808
dailin.ji <dailinji1212@gmail.com>
2022-06-16 18:59:18 -0400

    add data sync on S3

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,15 @@ import plotly.express as px
 from backtest import backtest_dashboard
 from realtime import realtime_dashboard
 from datapath import *
+import boto3
+import s3fs
+from io import StringIO
+
+
+client = boto3.client('s3', aws_access_key_id='AKIATHGO3RS3C7EMIAU4',
+        aws_secret_access_key='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')
+bucket_name = 'research-dashboard'
+
 
 #page config
 st.set_page_config(
@@ -26,12 +35,22 @@ with st.sidebar:
 
 #real time dashboard
 if selected == "Real-Time":
-    st.write(transaction_data_path,portfolio_value_path)
-    #realtime_dashboard(transaction_data_path,portfolio_value_path)
+    transaction_obj = client.get_object(Bucket=bucket_name, Key='trading-dashboard-data/purchase_info.csv')
+    transaction_body = transaction_obj['Body']
+    transaction_csv_string = transaction_body.read().decode('utf-8')
+
+    portfolio_obj = client.get_object(Bucket=bucket_name, Key='trading-dashboard-data/portfolio_returns.csv')
+    portfolio_body = portfolio_obj['Body']
+    portfolio_csv_string = portfolio_body.read().decode('utf-8')
+
+    transaction_data = pd.read_csv(StringIO(transaction_csv_string))
+    portfolio_return = pd.read_csv(StringIO(portfolio_csv_string))
+    print(portfolio_return.columns)
+    realtime_dashboard(portfolio_return,transaction_data)
 
 #backtest dashboard
 if selected == "Backtesting":
     st.write(backtest_data_path)
-    #backtest_dashboard(backtest_data_path)
+    backtest_dashboard(backtest_data_path)
 
    
--- realtime.py
+++ realtime.py
@@ -8,11 +8,7 @@ import plotly.express as px
 #rename columns for better format
 
 
-def realtime_dashboard(transaction_data_path,portfolio_value_path):
-    #read data
-    portfolio_value = pd.read_csv(portfolio_value_path)
-    transactions = pd.read_csv(transaction_data_path)
-
+def realtime_dashboard(portfolio_value, transactions):
     #Page title
     st.markdown("<h1 style='text-align: center;'>Real Time Results</h1>", unsafe_allow_html=True)
     st.text("Last updated: "+portfolio_value['dt'].iloc[-1])
@@ -27,20 +23,20 @@ def realtime_dashboard(transaction_data_path,portfolio_value_path):
 
     day_return =  portfolio_value['daily_return'].iloc[-1]
     week_return =  portfolio_value['weekly_return'].iloc[-1]
-    month_return =  portfolio_value['monthly_return'].iloc[-1]
+    month_return =  portfolio_value['monthly_return'].iloc[-2]
 
         
     with placeholder.container():
 
         m1, m2, m3 = st.columns(3)
 
-        m1.metric(label="Daily Return", value=str(round(day_return,4)*100)+"%")
-        m2.metric(label="Weekly Return", value=str(round(week_return,4)*100)+"%")
-        m3.metric(label="Monthly Return", value=str(round(month_return,4)*100)+"%")
+        m1.metric(label="Daily Return", value=str(day_return*100)+"%")
+        m2.metric(label="Weekly Return", value=str(week_return*100)+"%")
+        m3.metric(label="Monthly Return", value=str(month_return*100)+"%")
 
 
         st.markdown("### Portfolio " + graph_filter)
-        fig = px.line(portfolio_value.iloc[-20:],x = 'dt',y=graph_filter)
+        fig = px.line(portfolio_value.iloc[-20:],y=graph_filter)
         st.plotly_chart(fig, use_container_width=True)
 
         box1,box2 = st.columns(2)
@@ -48,10 +44,9 @@ def realtime_dashboard(transaction_data_path,portfolio_value_path):
             st.markdown("### Basket")
 
             basket_df = transactions.groupby(['yahoo_ticker'])[['total_shares_held']].last().reset_index()
-            basket_df = basket_df.loc[basket_df['total_shares_held'] != 0]
-            basket_df.columns = ['commodity','shares held']
+            basket_df = basket_df.loc[basket_df['total_shares_held'] > 0]
             box1.dataframe(basket_df, width = 300,height= 3000)
         with box2:
             st.markdown("### Transactions")
-            box2.table(transactions.tail(5).astype(str))
+            box2.table(transactions[['date','action','company','num_shares','total_usd']].tail(5).astype(str))
         
