976457e0846318b8dd5faf2461327220f5568237
TigaJi <dailinj@outlook.com>
2022-06-15 22:20:02 -0400

    Update datapath.py

--- datapath.py
+++ datapath.py
@@ -1,4 +1,4 @@
-transaction_data_path = '../sample_data/purchase_info.csv'
-backtest_data_path = '../sample_data/backtest_daily_result_sample.csv'
-portfolio_value_path = '../sample_data/sample_portfolio_value.csv'
+transaction_data_path = 'sample_data/purchase_info.csv'
+backtest_data_path = 'sample_data/backtest_daily_result_sample.csv'
+portfolio_value_path = 'sample_data/sample_portfolio_value.csv'
 
